840bb98381ff3649361c4c3d2cc40b02bbb7cc2f
tunukumarsahoo <rs_saheed@outlook.com>
2021-01-08 20:40:42 +0530

    for git run - saheed

---
+++ test_git_jenkin/src/main/java/com/sah/test_git_jenkin/App.java
@@ -0,0 +1,13 @@
+package com.sah.test_git_jenkin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
